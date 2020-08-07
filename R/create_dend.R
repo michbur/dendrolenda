cd5f46ba2127a98943c9fe4a4408b8d922abcac4
Michał Burdukiewicz <michalburdukiewicz@gmail.com>
2020-08-07 18:18:26 +0200

    added spellcheck

--- R/create_dend.R
+++ R/create_dend.R
@@ -5,7 +5,7 @@
 #' @importFrom tidyr pivot_wider
 #' @importFrom dplyr %>% select
 create_dend <- function(x, col_name1, col_name2) {
-  pivot_wider(dat, names_from = c(col_name2),
+  pivot_wider(x, names_from = c(col_name2),
               values_from = "value") %>%
     as.data.frame() %>% {
       rownames(.) <- .[[col_name1]]
--- R/dendrolenda-package.R
+++ R/dendrolenda-package.R
@@ -1,1 +1,4 @@
 #' @importFrom stats as.dendrogram dist hclust nobs
+
+if(getRversion() >= "2.15.1")
+  utils::globalVariables(c("x", "y", "xend", "yend"))
