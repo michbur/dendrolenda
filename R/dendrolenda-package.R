5b78b6c574cacd9ff040c386625d955aaffb965e
ksidorczuk <sidorczuk.katarzyna17@gmail.com>
2020-08-08 16:24:52 +0200

    updated dendrolenda-package.R

--- R/dendrolenda-package.R
+++ R/dendrolenda-package.R
@@ -1,4 +1,23 @@
+#' dendrolenda
+#' 
+#' dendrolenda package allows easy visualization of heatmaps with dendrograms.
+#' The main function \code{\link{arrange_dendrogram}} aligns dendrograms
+#' with heatmap, so manual arrangement of plots is not necessary!
+#' 
+#' @section Happy Birthday, Rafal!:
+#' We know how many heatmaps you plot \cr
+#' and their arrangement can take a lot. \cr
+#' So to save you some time and nerves \cr
+#' we built a package our friend deserves. \cr
+#' Now you can efficiently plot an test. \cr
+#' We wish you all the best!
+#' 
+#' 
 #' @importFrom stats as.dendrogram dist hclust nobs
-
+#' @author Michal Burdukiewicz, Katarzyna Sidorczuk, Stefan Roediger
+#' @docType package
+#' @name dendrolenda-package
+#' @aliases dendrolenda
+#' 
 if(getRversion() >= "2.15.1")
   utils::globalVariables(c("x", "y", "xend", "yend"))
